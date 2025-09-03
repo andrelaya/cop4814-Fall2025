5dfaae6d1bce3759edf065ab4ea5dc4d99cfb0aa
andrelaya <andreenriquelaya@gmail.com>
2025-09-03 15:08:20 -0400

    finished the examples of section 1

--- pythonSyntax.py
+++ pythonSyntax.py
@@ -31,8 +31,18 @@ block comment
 # - Type checking with type()
 # ==========================================================
 
+message = "Welcome to FIU"
+print(type(message)) #type() is a function too
+
+a = 10
+b = 2
+print(type(a),type(b))
+
+isOpen = True
+print(type(isOpen))
 
 
+print(message[8])
 # ==========================================================
 # 3. BASIC OPERATORS
 # ----------------------------------------------------------
@@ -40,6 +50,15 @@ block comment
 # - Comparison (==, !=, >, <, >=, <=)
 # - Logical (and, or, not)
 # ==========================================================
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(7/2)
+print(7//2) #integer division
+print(a**b) #exponential
+print(7%2) #modulus (remainder of the divison)
 
 
 
@@ -51,7 +70,16 @@ block comment
 # - Common methods (.upper(), .lower(), .strip(), etc.)
 # ==========================================================
 
+first_name = "Andre"
+last_name = "Laya"
 
+print(first_name + last_name)
+print(first_name + " " + last_name)
+print(first_name, last_name)
+
+print(f"My name is {first_name.upper()} {last_name.title()}.")
+print("2" + "3")
+print("***Welcome to Software Dev***".strip('*'))
 
 # ==========================================================
 # 5. LISTS
@@ -64,12 +92,47 @@ block comment
 
 
 
+professors = ["greg", "richard", "kianoosh", "debra", "jason", "leo", "heather"]
+print(type(professors))
+print(professors[0])
+print(professors[-1])
+print(professors[2:5]) # accessing indices 2, 3, and 4
+print(professors[:5]) # accessing from index 0 to 4
+print(professors[3:]) # accessing from 3 to the end
+print(professors[:])
+
+
+professors.append("todd")
+print(professors)
+professors.extend(["michael", "mustafa", "naomi"])
+print(professors)
+professors.insert(2,"vyoma")
+professors.remove("greg")
+print(professors)
+x=professors.pop(2)
+print(professors, x)
+professors.reverse()
+print(professors)
+
+professors.sort()
+print(professors)
+professors.sort(reverse=True)
+print(professors)
+
 # ==========================================================
 # 6. TUPLES AND SETS
 # ----------------------------------------------------------
 # - Tuples: immutable sequences
 # - Sets: unique collections
 # ==========================================================
+
+grades = (88.3, 78.6, 99.5)
+print(type(grades))
+#grades[0] = 91.3
+
+members = {"greg", "richard", "greg"}
+print(members) # this is going to ve the answer of a future assignment -----------
+
 
 
 
